38b81d5b90a762ecf2e43f630c0239003ea35ac1
HP 250g7 <brightology2@gmain.com>
2023-07-17 01:21:55 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ListEmployeeComponent from './component/ListEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <ListEmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
